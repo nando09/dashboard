e1e792244d9208f1250ba463d031256f905a06dc
Fernando Batista De Oliveira <Fernando Batista@FERNANDO-DEV.EXPERTVOICE.LOCAL>
2021-05-27 18:10:35 -0300

    Continue dashboard

--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -26,6 +26,34 @@
                     <BellIcon class="w-5 h-5 ml-8 opacity-50 hover:opacity-100 cursor-pointer transition duration-100"/>
                 </div>
             </header>
+
+            <h1 class="font-bold text-4xl mt-6 mb-4">Discover</h1>
+
+            <div class="grid gap-4 grid-cols-12">
+                <div class="col-span-8 h-96 rounded-3xl overflow-hidden relative">
+                    <div class="flex w-full h-full">
+                        <img src="https://via.placeholder.com/2000X1400" class="object-cover w-full">
+                    </div>
+                    <div class="z-10 absolute top-0 p-6 w-full h-full">
+                        <h3 class="text-2xl w-52">How to do Basic Jumping and how to landing safely</h3>
+                        <div class="absolute right-6 bottom-6 px-2 py-1 bg-gray-800 bg-opacity-50 text-white rounded-lg">
+                            7 Minutes
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-span-4 h-96 rounded-3xl overflow-hidden relative">
+                    <div class="flex w-full h-full">
+                        <img src="https://via.placeholder.com/2000X1400" class="object-cover w-full">
+                    </div>
+                    <div class="z-10 absolute top-0 p-6 w-full h-full">
+                        <h3 class="text-2xl max-w-xs">How to do Basic Jumping and how to landing safely</h3>
+                        <div class="absolute right-6 bottom-6 px-2 py-1 bg-gray-800 bg-opacity-50 text-white rounded-lg">
+                            7 Minutes
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
