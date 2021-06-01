bbe8c004f3a4c3345976308fd7c2ac0fdb253392
Fernando Batista De Oliveira <Fernando Batista@FERNANDO-DEV.EXPERTVOICE.LOCAL>
2021-06-01 19:02:06 -0300

    Init register user layout

---
+++ src/components/ModalRegister.vue
@@ -0,0 +1,167 @@
+<template>
+    <div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-first-name"
+            >
+                Nome
+            </label>
+            <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="grid-first-name"
+                type="text"
+                v-model="nome"
+                placeholder="Jane"
+            />
+            <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+            <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+            >
+                Sobrenome
+            </label>
+            <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                v-model="sobrenome"
+                placeholder="Doe"
+            />
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+            <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-password"
+            >
+                Senha
+            </label>
+            <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                v-model="senha"
+                type="password"
+                placeholder="******************"
+            />
+            <p class="text-gray-600 text-xs italic">
+                Make it as long and as crazy as you'd like
+            </p>
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+            <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-password"
+            >
+                Email
+            </label>
+            <input
+                v-model="email"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                type="password"
+                placeholder="janedoe@gmail.com"
+            />
+            <p class="text-gray-600 text-xs italic">
+                Make it as long and as crazy as you'd like
+            </p>
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-2">
+            <div class="w-full md:w-2/3 px-3 mb-6 md:mb-0">
+            <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-password"
+            >
+                Usuário
+            </label>
+            <input
+                v-model="usuario"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                type="password"
+                placeholder="jone_doe"
+            />
+            <p class="text-gray-600 text-xs italic">
+                Make it as long and as crazy as you'd like
+            </p>
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-state"
+            >
+                Perfil
+            </label>
+            <div class="relative">
+                <select
+                class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-state"
+                v-model="perfil"
+                >
+                <option>New Mexico</option>
+                <option>Missouri</option>
+                <option>Texas</option>
+                </select>
+                <div
+                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+                >
+                <svg
+                    class="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                >
+                    <path
+                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                    />
+                </svg>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-around">
+        <button
+        type="button"
+        class="inline-flex justify-center px-4 py-2 text-sm font-medium text-green-900 bg-green-100 border border-transparent rounded-md hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-green-500"
+        @click="cadastrar"
+        >
+        Cadastrar
+        </button>
+
+        <button
+        type="button"
+        class="inline-flex justify-center px-4 py-2 text-sm font-medium text-red-900 bg-red-100 border border-transparent rounded-md hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
+        @click="$emit('close')"
+        >
+        Cancelar
+        </button>
+    </div>
+</template>
+<script lang="ts" setup>
+    const nome = '';
+    const sobrenome = '';
+    const senha = '';
+    const email = '';
+    const usuario = '';
+    const perfil = '';
+
+    function cadastrar(){
+        let data = {
+            nome:       nome,
+            sobrenome:  sobrenome,
+            senha:      senha,
+            email:      email,
+            usuario:    usuario,
+            perfil:     perfil            
+        };
+
+        $root.$emit('input', data);
+    }
+</script>
--- src/layouts/contents/Settings/ContentSetting.vue
+++ src/layouts/contents/Settings/ContentSetting.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="grid grid-cols-12 grid-rows-6">
-
         <div class="col-span-12 row-span-6">
-            <div class="flex sm:flex-row flex-col mb-6">
+            <div class="flex mb-6 relative">
                 <div class="flex flex-row sm:mb-0">
                     <div class="relative">
                         <select
@@ -21,9 +20,9 @@
                     <div class="relative">
                         <select
                             class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-                            <option>All</option>
-                            <option>Active</option>
-                            <option>Inactive</option>
+                            <option>Todos</option>
+                            <option>Ativados</option>
+                            <option>Desativados</option>
                         </select>
                         <div
                             class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -41,8 +40,16 @@
                             </path>
                         </svg>
                     </span>
-                    <input placeholder="Search"
+                    <input placeholder="Nome do usuário"
                         class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+                </div>
+                <div class='absolute right-0'>
+                    <button
+                        @click="openModal"
+                        class="p-2 bg-orange-400 rounded text-white focus:outline-none"
+                    >
+                        Cadastrar
+                    </button>
                 </div>
             </div>
             <div class="">
@@ -217,6 +224,220 @@
             </div>
         </div>
 
+        <TransitionRoot appear :show="isOpen" as="template">
+            <Dialog as="div" @close="closeModal">
+            <div class="fixed inset-0 z-10 overflow-y-auto">
+                <div class="min-h-screen px-4 text-center">
+                <DialogOverlay class="fixed inset-0 bg-black opacity-50" />
 
+                <span class="inline-block h-screen align-middle" aria-hidden="true">
+                    &#8203;
+                </span>
+
+                <TransitionChild
+                    as="template"
+                    enter="duration-300 ease-out"
+                    enter-from="opacity-0 scale-95"
+                    enter-to="opacity-100 scale-100"
+                    leave="duration-200 ease-in"
+                    leave-from="opacity-100 scale-100"
+                    leave-to="opacity-0 scale-95"
+                >
+                    <div class="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                        <div>
+                            <div class="flex flex-wrap -mx-3 mb-6">
+                                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label
+                                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                    for="grid-first-name"
+                                >
+                                    Nome
+                                </label>
+                                <input
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                    id="grid-first-name"
+                                    type="text"
+                                    v-model="nome"
+                                    placeholder="Jane"
+                                />
+                                <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+                                </div>
+                                <div class="w-full md:w-1/2 px-3">
+                                <label
+                                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                    for="grid-last-name"
+                                >
+                                    Sobrenome
+                                </label>
+                                <input
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="grid-last-name"
+                                    type="text"
+                                    v-model="sobrenome"
+                                    placeholder="Doe"
+                                />
+                                </div>
+                            </div>
+                            <div class="flex flex-wrap -mx-3 mb-6">
+                                <div class="w-full px-3">
+                                <label
+                                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                    for="grid-password"
+                                >
+                                    Senha
+                                </label>
+                                <input
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="grid-password"
+                                    v-model="senha"
+                                    type="password"
+                                    placeholder="******************"
+                                />
+                                <p class="text-gray-600 text-xs italic">
+                                    Make it as long and as crazy as you'd like
+                                </p>
+                                </div>
+                            </div>
+                            <div class="flex flex-wrap -mx-3 mb-6">
+                                <div class="w-full px-3">
+                                <label
+                                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                    for="grid-password"
+                                >
+                                    Email
+                                </label>
+                                <input
+                                    v-model="email"
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="grid-password"
+                                    type="password"
+                                    placeholder="janedoe@gmail.com"
+                                />
+                                <p class="text-gray-600 text-xs italic">
+                                    Make it as long and as crazy as you'd like
+                                </p>
+                                </div>
+                            </div>
+                            <div class="flex flex-wrap -mx-3 mb-2">
+                                <div class="w-full md:w-2/3 px-3 mb-6 md:mb-0">
+                                <label
+                                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                    for="grid-password"
+                                >
+                                    Usuário
+                                </label>
+                                <input
+                                    v-model="usuario"
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="grid-password"
+                                    type="password"
+                                    placeholder="jone_doe"
+                                />
+                                <p class="text-gray-600 text-xs italic">
+                                    Make it as long and as crazy as you'd like
+                                </p>
+                                </div>
+                                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                <label
+                                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                    for="grid-state"
+                                >
+                                    Perfil
+                                </label>
+                                <div class="relative">
+                                    <select
+                                    class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="grid-state"
+                                    v-model="perfil"
+                                    >
+                                    <option>New Mexico</option>
+                                    <option>Missouri</option>
+                                    <option>Texas</option>
+                                    </select>
+                                    <div
+                                    class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+                                    >
+                                    <svg
+                                        class="fill-current h-4 w-4"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                    >
+                                        <path
+                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                                        />
+                                    </svg>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex justify-around">
+                            <button
+                            type="button"
+                            class="inline-flex justify-center px-4 py-2 text-sm font-medium text-green-900 bg-green-100 border border-transparent rounded-md hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-green-500"
+                            @click="cadastrar"
+                            >
+                            Cadastrar
+                            </button>
+
+                            <button
+                            type="button"
+                            class="inline-flex justify-center px-4 py-2 text-sm font-medium text-red-900 bg-red-100 border border-transparent rounded-md hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
+                            @click="closeModal"
+                            >
+                            Cancelar
+                            </button>
+                        </div>
+                    </div>
+                </TransitionChild>
+                </div>
+            </div>
+            </Dialog>
+        </TransitionRoot>
     </div>
 </template>
+<script lang="ts" setup>
+    import ModalRegister from '../../../components/ModalRegister.vue';
+    import { ref } from 'vue'
+    import {
+        TransitionRoot,
+        TransitionChild,
+        Dialog,
+        DialogOverlay,
+        DialogTitle,
+    } from '@headlessui/vue'
+
+    const nome = '';
+    const sobrenome = '';
+    const senha = '';
+    const email = '';
+    const usuario = '';
+    const perfil = '';
+
+    const isOpen = ref(false);
+
+    function cadastrar(){
+        let data = {
+            nome:       nome,
+            sobrenome:  sobrenome,
+            senha:      senha,
+            email:      email,
+            usuario:    usuario,
+            perfil:     perfil            
+        };
+
+        console.log(data);
+    }
+
+    function closeModal() {
+        isOpen.value = false;
+    }
+
+    function openModal() {
+        isOpen.value = true;
+    }
+
+    function adicionarQuantidade(){
+        console.log('Neh que deu certo!');
+    }
+</script>
